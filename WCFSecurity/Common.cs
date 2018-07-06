993b668f47c2ab9267c88b84094e365ec7d47e0d
OSCAR ANDRES CESPEDES DIAZ <oscar.cespedes@correo.policia.gov.co>
2018-07-06 11:16:47 -0500

    Add project files.

---
+++ WCFSecurity/Common.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace WCFSecurity
+{
+    public class Common
+    {
+        public string GenerateSHA256(string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            SHA256Managed sHA256 = new SHA256Managed();
+            byte[] hash = sHA256.ComputeHash(bytes);
+
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+                hashString += String.Format("{0:x2}", x);
+
+            return hashString;
+        }
+
+        #region RindJael
+
+        public string GenerateKey()
+        {
+            SymmetricAlgorithm symmetricAlgorithm = new RijndaelManaged
+            {
+                KeySize = 128
+            };
+            symmetricAlgorithm.GenerateKey();
+
+            return Convert.ToBase64String(symmetricAlgorithm.Key);
+        }
+
+
+        public string Encrypt(string message, string key)
+        {
+            SymmetricAlgorithm symmetricAlgorithm = new RijndaelManaged
+            {
+                KeySize = 128
+            };
+            symmetricAlgorithm.Key = Convert.FromBase64String(key);
+            symmetricAlgorithm.Mode = CipherMode.CTS;
+
+            ICryptoTransform cryptoTransform = symmetricAlgorithm.CreateEncryptor();
+
+            Byte[] data = Encoding.UTF8.GetBytes(message);
+
+            var dataEncrypted = cryptoTransform.TransformFinalBlock(data , 0 , data.Length);
+
+            return Convert.ToBase64String(dataEncrypted);
+        }
+
+
+        //public string Decrypt(string message, string key)
+        //{
+        //    SymmetricAlgorithm symmetricAlgorithm = new RijndaelManaged
+        //    {
+        //        KeySize = 128
+        //    };
+        //    symmetricAlgorithm.Key = Convert.FromBase64String(key);
+        //    symmetricAlgorithm.Mode = CipherMode.CTS;
+
+        //    ICryptoTransform cryptoTransform = symmetricAlgorithm.CreateDecryptor();
+
+        //    Byte[] data = Encoding.UTF8.GetBytes(message);
+
+        //    var dataEncrypted = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+
+        //    return Convert.ToBase64String(dataEncrypted);
+        //}
+
+        #endregion
+
+    }
+}
---
+++ WCFSecurity/ISecurity.cs
@@ -0,0 +1,52 @@
+﻿namespace WCFSecurity
+{
+    using System;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+
+    [ServiceContract]
+    public interface ISecurity
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "GetToken", ResponseFormat = WebMessageFormat.Json)]
+        string GetToken(string username, string password);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Create", ResponseFormat = WebMessageFormat.Json)]
+        ResponseModel CreateUser(string username, string password);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "ChagePassword", ResponseFormat = WebMessageFormat.Json)]
+        ResponseModel ChagePassword(string username, string actualPassword, string newPassword);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "CreateRole", ResponseFormat = WebMessageFormat.Json)]
+        ResponseModel AddRole(string username, string role);
+
+    }
+
+    [DataContract]
+    public class ResponseModel
+    {
+        [DataMember]
+        public bool IsError => Exception != null;
+
+        [DataMember]
+        public Exception Exception { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+    }
+
+
+    public class TokenSecurityModel
+    {
+        public Guid Id { get; set; }
+        public DateTime Expiration { get; set; }
+        public string[] Roles { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+}
